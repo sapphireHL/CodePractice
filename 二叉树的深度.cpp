4248cd6270a9cce4a46ff74c45fa7d0c4bd6e26c
sapphireHL <sapphire57866@gmail.com>
2018-03-30 10:17:44 +0800

    递归

---
+++ 二叉树的深度.cpp
@@ -0,0 +1,22 @@
+/*
+struct TreeNode {
+	int val;
+	struct TreeNode *left;
+	struct TreeNode *right;
+	TreeNode(int x) :
+			val(x), left(NULL), right(NULL) {
+	}
+};*/
+class Solution {
+public:
+    int TreeDepth(TreeNode* pRoot)
+    {
+        if(pRoot){
+            int left = TreeDepth(pRoot->left);
+            int right = TreeDepth(pRoot->right);
+            return max(left, right) + 1;
+        }
+        else
+            return 0;
+    }
+};
