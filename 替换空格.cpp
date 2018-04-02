044021e82c7e8d61ac3de105b26cec098f06fe9f
sapphireHL <sapphire57866@gmail.com>
2018-04-02 10:03:49 +0800

    字符串

---
+++ 替换空格.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+	void replaceSpace(char *str,int length) {
+        string s;
+        for(int i = 0; i < length; i++){
+            if(str[i] == ' '){
+                s += "%20";
+            }
+            else
+                s += str[i];
+        }
+        strcpy(str, s.c_str());
+	}
+};
